5483f41db56067703af8ae906b762bf337c541d5
Edwin Salazar <edwsalaz@cisco.com>
2024-06-14 14:22:43 -0500

    httptest

--- peruvian/testshttp.tf
+++ peruvian/testshttp.tf
@@ -1,1 +1,14 @@
-
+resource "thousandeyes_http_server" "http_server_app.thousandeyes.com" {
+  test_name =  "app.thousandeyes.com HTTP"
+  description    = "by terraform at ${local.now}"
+  interval       = var.test_interval
+  alerts_enabled = var.alerts
+  url = "https://www.thousandeyes.com"
+  bgp_measurements = var.bgp
+ dynamic "agents" {
+    for_each = local.agentId
+    content  {
+    agent_id   = agents.value
+    }
+  }
+}
