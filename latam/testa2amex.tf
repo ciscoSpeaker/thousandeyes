d9f9f83f1e2674aadfdaf8c4ff665c5a63ce680a
Edwin Salazar <edwsalaz@cisco.com>
2024-06-14 17:32:09 -0500

    mex

--- latam/testa2amex.tf
+++ latam/testa2amex.tf
@@ -25,17 +25,3 @@ resource "thousandeyes_agent_to_agent" "agent_to_agent_MexicoCity_Guadalajara" {
     agent_id = 63438 
   }
 }
-
-resource "thousandeyes_agent_to_agent" "agent_to_agent_MexicoCity_Dallas" {
-  test_name      = "MexicoCity-to-Dallas"
-  description    = "by terraform"
-  interval       = var.test_interval
-  direction = "BIDIRECTIONAL"
-  protocol = "TCP"
-  target_agent_id = "26"
-  bgp_measurements = var.bgp
-  alerts_enabled = var.alerts
-  agents {
-    agent_id = 63438 
-  }
-}
