161827e94d6c82d957ac6f93c487794a30b758dd
Edwin Salazar <edwsalaz@cisco.com>
2024-06-14 15:02:53 -0500

    delete

--- latam/testsagenttoagent.tf
+++ latam/testsagenttoagent.tf
@@ -26,27 +26,13 @@ resource "thousandeyes_agent_to_agent" "agent_to_agent_MexicoCity_Guadalajara" {
   }
 }
 
-resource "thousandeyes_agent_to_agent" "agent_to_agent_MexicoCity_Raleigh" {
-  test_name      = "MexicoCity-to-Raleigh"
-  description    = "by terraform at"
-  interval       = var.test_interval
-  direction = "BIDIRECTIONAL"
-  protocol = "TCP"
-  target_agent_id = "69"
-  bgp_measurements = var.bgp
-  alerts_enabled = var.alerts
-  agents {
-    agent_id = 63438 
-  }
-}
-
 resource "thousandeyes_agent_to_agent" "agent_to_agent_MexicoCity_Dallas" {
   test_name      = "MexicoCity-to-Dallas"
   description    = "by terraform at"
   interval       = var.test_interval
   direction = "BIDIRECTIONAL"
   protocol = "TCP"
-  target_agent_id = "26"
+  target_agent_id = "26kam"
   bgp_measurements = var.bgp
   alerts_enabled = var.alerts
   agents {
