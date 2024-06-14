5e724e85faa5c46f558cd530f035ecf258ff2828
Edwin Salazar <edwsalaz@cisco.com>
2024-06-14 13:33:22 -0500

    add

---
+++ peruvian/variables.tf
@@ -0,0 +1,27 @@
+variable "te_token" {
+  description = "ThousandEyes OAuth authentication token"
+  type = string
+  sensitive = true
+}
+
+variable "te_aid" {
+  description = "Account Group ID"
+  type  = string
+}
+
+variable "te_timeout" {
+  description = "The timeout value."
+  type  = number
+}
+
+variable "test_interval" {
+  type  = number
+}
+
+variable "alerts" {
+  type  = string
+}
+
+variable "bgp" {
+  type  = string
+}
