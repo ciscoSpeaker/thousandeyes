194aefa776b134eefca301bd26777271aac959b2
Edwin Salazar <edwsalaz@cisco.com>
2024-06-14 17:25:04 -0500

    cloud agents

---
+++ latam/cloudagents.tf
@@ -0,0 +1,10 @@
+locals {
+  cloudagents = jsondecode(file("${path.module}/cloudagents.json")) 
+  agents_name_id = {for ag_name in local.cloudagents.agents : ag_name.agentName => ag_name.agentId}
+  agentName = keys(local.agents_name_id)
+  agentId = values(local.agents_name_id) 
+  }
+
+output "cloud_agents" {
+  value = local.agents_name_id
+}
