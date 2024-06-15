d29d6493e7825a90e432acbb1d256ed77d18e133
Edwin Salazar <edwsalaz@cisco.com>
2024-06-15 17:15:22 -0500

    deleteDNStrace

--- peruvian/tests.tf
+++ peruvian/tests.tf
@@ -18,17 +18,17 @@ resource "thousandeyes_http_server" "http" {
   }
 }
 
-resource "thousandeyes_dns_trace" "dns" {
-  for_each = tomap({ for inst in local.tests : inst.test_resource => inst })
-  test_name      = each.value.test_name
-  description    = "by terraform"
-  interval       = var.test_interval
-  alerts_enabled = var.alerts
-  domain = each.value.test_domain
-  dynamic "agents" {
-    for_each = local.agentId
-    content  {
-    agent_id   = agents.value
-    }
-  }
-}
+#resource "thousandeyes_dns_trace" "dns" {
+#  for_each = tomap({ for inst in local.tests : inst.test_resource => inst })
+#  test_name      = each.value.test_name
+#  description    = "by terraform"
+#  interval       = var.test_interval
+#  alerts_enabled = var.alerts
+#  domain = each.value.test_domain
+#  dynamic "agents" {
+#    for_each = local.agentId
+#    content  {
+#    agent_id   = agents.value
+#    }
+#  }
+#}
