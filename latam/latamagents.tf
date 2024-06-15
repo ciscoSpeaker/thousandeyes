2e28a9051bf0120276bebf323c3e4906b3fda6ce
Edwin Salazar <edwsalaz@cisco.com>
2024-06-15 18:45:28 -0500

    add

--- latam/latamagents.tf
+++ latam/latamagents.tf
@@ -1,8 +1,8 @@
 locals {
   latamAgents = csvdecode(file("${path.module}/latamagents.csv"))
-  latamAgents_name_id = {for ag_name in local.latamAgents : ag_name.agent_id => ag_name}
+  latamAgents_name_id = {for ag_name in local.latamAgents : ag_name.agent_id => ag_name.agent_country}
   latamAgentId = keys(local.latamAgents_name_id)
-  latamAgentCountry = values(local.latamAgents_name_id.agent_country) 
+  latamAgentCountry = values(local.latamAgents_name_id) 
   }
 
 output "latam_agents" {
