ab548eded248d356562007efe89840292b7aad88
Edwin Salazar <edwsalaz@cisco.com>
2024-06-15 18:37:37 -0500

    update

--- peruvian/dns.tf
+++ peruvian/dns.tf
@@ -1,10 +1,10 @@
 locals {
   server_dns = csvdecode(file("${path.module}/dns.csv"))
-  server_dns_name_id = {for dns in local.server_dns : dns.dns => dns.dns}
-  server_dns_name = keys(local.server_dns_name_id)
-  server_dns_id = values(local.server_dns_name_id)
+  server_dns_id_dns = {for dns in local.server_dns : dns.id => dns.dns}
+  server_dns_id = keys(local.server_dns_id_dns)
+  server_dns_dns = values(local.server_dns_id_dns)
 }
 
 output "server_dns" {
-  value = local.server_dns_name_id
+  value = local.server_dns_id_dns
 }
