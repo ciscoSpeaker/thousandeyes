72896aa6c652ef61e5262cefaa44b99c1c83b802
Edwin Salazar <edwsalaz@cisco.com>
2024-06-18 03:50:10 -0500

    Update tests.tf

--- peruvian/tests.tf
+++ peruvian/tests.tf
@@ -18,27 +18,27 @@ resource "thousandeyes_http_server" "http" {
   }
 }
 
-resource "thousandeyes_dns_server" "dns" {
-  for_each = tomap({ for inst in local.tests : inst.test_resource => inst })
-  test_name      = each.value.test_name
-  description    = "by terraform"
-  interval       = var.test_interval
-  alerts_enabled = var.alerts
-  domain = each.value.test_domain
-  dynamic "dns_servers" {
-    for_each = local.server_dns_id_dns
-    content {
-      server_name = dns_servers.value
-    }
-  }
-  dynamic "agents" {
-    for_each = local.agentId
-    content  {
-    agent_id   = agents.value
-    }
-  }
-}
-
+#resource "thousandeyes_dns_server" "dns" {
+  #for_each = tomap({ for inst in local.tests : inst.test_resource => inst })
+  #test_name      = each.value.test_name
+  #description    = "by terraform"
+  #interval       = var.test_interval
+  #alerts_enabled = var.alerts
+  #domain = each.value.test_domain
+  #dynamic "dns_servers" {
+    #for_each = local.server_dns_id_dns
+    #content {
+      #server_name = dns_servers.value
+    #}
+  #}
+  #dynamic "agents" {
+    #for_each = local.agentId
+    #content  {
+    #agent_id   = agents.value
+    #}
+  #}
+#}
+#
 #resource "thousandeyes_dns_trace" "dns" {
 #  for_each = tomap({ for inst in local.tests : inst.test_resource => inst })
 #  test_name      = each.value.test_name
