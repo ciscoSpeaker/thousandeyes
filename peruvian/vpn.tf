391b14b4bf481e3e1fb488e59a605090f05e256f
Edwin Salazar <edwsalaz@cisco.com>
2024-06-21 13:15:52 -0500

    vpnfixing

--- peruvian/vpn.tf
+++ peruvian/vpn.tf
@@ -3,10 +3,11 @@ locals {
   }
 
 resource "thousandeyes_agent_to_server" "vpn" {
-  for_each = tomap({ for inst in local.vpn : inst.vpn_ip => inst })
+  for_each = tomap({ for inst in local.vpn : inst.vpn_resource => inst })
   test_name =  each.value.vpn_name
   server = each.value.vpn_target
   protocol = each.value.protocol
+  port = each.value.port
   interval  = var.test_vpn_interval
   alerts_enabled = var.alerts
   bgp_measurements = var.bgp
