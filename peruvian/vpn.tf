304243fca06c414d7b1d03d75d1d26e80af0b0b3
Edwin Salazar <edwsalaz@cisco.com>
2024-06-20 22:03:14 -0500

    vpn update

---
+++ peruvian/vpn.tf
@@ -0,0 +1,19 @@
+locals {
+  vpn = csvdecode(file("${path.module}/vpn.csv"))
+  }
+
+resource "thousandeyes_agent_to_server" "vpn" {
+  for_each = tomap({ for inst in local.vpn : inst.vpn_ip => inst })
+  test_name =  each.value.vpn_name
+  server = each.value.vpn_target
+  protocol = each.value.protocol
+  interval  = var.test_vpn_interval
+  alerts_enabled = var.alerts
+  bgp_measurements = var.bgp
+  dynamic "agents" {
+    for_each = local.agentId
+    content  {
+    agent_id   = agents.value
+    }
+  }
+}
