61b091b13504a902a05ad67e7899d2134e642ae7
Edwin Salazar <edwsalaz@cisco.com>
2024-06-26 18:29:25 -0500

    Update tests.tf

--- latam/tests.tf
+++ latam/tests.tf
@@ -31,3 +31,20 @@ resource "thousandeyes_agent_to_agent" "Mexico_City" {
     }
   }
 }
+
+resource "thousandeyes_agent_to_agent" "Santiago" {
+  for_each = tomap({ for inst in local.agentsFromSantiago : inst.agent_name => inst })
+  test_name =  "Santiago to ${each.value.agent_name}"
+  target_agent_id = each.value.agent_id
+  interval  = var.test_A2A_interval
+  direction = var.test_A2A_direction
+  protocol = var.test_A2A_protocol
+  bgp_measurements = var.bgp
+  alerts_enabled = var.alerts
+  dynamic "agents" {
+    for_each = local.agentSantiago_id
+    content  {
+    agent_id   = agents.value
+    }
+  }
+}
