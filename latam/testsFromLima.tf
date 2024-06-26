1c8488b4e3b4fba898bc60b4823491ad2b4b021b
Edwin Salazar <edwsalaz@cisco.com>
2024-06-26 12:30:05 -0500

    Update testsFromLima.tf

--- latam/testsFromLima.tf
+++ latam/testsFromLima.tf
@@ -2,7 +2,7 @@ resource "thousandeyes_agent_to_agent" "Lima" {
   for_each = tomap({ for inst in local.agentsFromLima : inst.agent_name => inst })
   test_name =  "Lima to - ${each.value.agent_name}"
   interval  = var.test_interval
-  target_agent_id = each.value.agend_id
+  target_agent_id = each.value.agent_id
   direction = "BIDIRECTIONAL"
   protocol = "TCP"
   bgp_measurements = var.bgp
