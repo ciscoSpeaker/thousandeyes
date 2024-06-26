b7aa918a4a27988f90d890424b5f2bfea35ef6ca
Edwin Salazar <edwsalaz@cisco.com>
2024-06-26 12:12:41 -0500

    lima

--- latam/testsFromLima.tf
+++ latam/testsFromLima.tf
@@ -1,8 +1,8 @@
 resource "thousandeyes_agent_to_agent" "Lima" {
-  for_each = tomap({ for inst in local.latamAgentsFromLima_name_id : inst.agent_id => inst })
+  for_each = tomap({ for inst in local.latamAgentsFromLima_name_id : inst.latamAgentFromLimaId => inst })
   test_name =  "Lima to - ${each.value.agent_name}"
   interval  = var.test_interval
-  target_agent_id = each.value.agent_id
+  target_agent_id = each.value.latamAgentFromLimaId
   direction = "BIDIRECTIONAL"
   bgp_measurements = var.bgp
   alerts_enabled = var.alerts
