7a4cdead7a5486c840ba7d04ca4fec7073ac3d34
Edwin Salazar <edwsalaz@cisco.com>
2024-06-26 12:40:25 -0500

    fixing

---
+++ latam/tests.tf
@@ -0,0 +1,25 @@
+resource "thousandeyes_agent_to_agent" "agent_to_agent_MexicoCity_Lima" {
+  test_name      = "MexicoCity-to-Lima"
+  interval       = var.test_interval
+  direction = "BIDIRECTIONAL"
+  protocol = "TCP"
+  target_agent_id = "66222"
+  bgp_measurements = var.bgp
+  alerts_enabled = var.alerts
+  agents {
+    agent_id = 63438 
+  }
+}
+
+resource "thousandeyes_agent_to_agent" "agent_to_agent_MexicoCity_Guadalajara" {
+  test_name      = "MexicoCity-to-Guadalajara"
+  interval       = var.test_interval
+  direction = "BIDIRECTIONAL"
+  protocol = "TCP"
+  target_agent_id = "320"
+  bgp_measurements = var.bgp
+  alerts_enabled = var.alerts
+  agents {
+    agent_id = 63438 
+  }
+}
