f685eb8ef1dd2a9f43f6a19bd2a93ddb97884a97
Edwin Salazar <edwsalaz@cisco.com>
2024-06-26 13:20:07 -0500

    adding Mexico City

---
+++ latam/tests.tf
@@ -0,0 +1,33 @@
+resource "thousandeyes_agent_to_agent" "Lima" {
+  for_each = tomap({ for inst in local.agentsFromLima : inst.agent_name => inst })
+  test_name =  "Lima to ${each.value.agent_name}"
+  target_agent_id = each.value.agent_id
+  interval  = var.test_A2A_interval
+  direction = var.test_A2A_direction
+  protocol = var.test_A2A_protocol
+  bgp_measurements = var.bgp
+  alerts_enabled = var.alerts
+  dynamic "agents" {
+    for_each = local.agentLima_id
+    content  {
+    agent_id   = agents.value
+    }
+  }
+}
+
+resource "thousandeyes_agent_to_agent" "Mexico_City" {
+  for_each = tomap({ for inst in local.agentsFromMexicoCity : inst.agent_name => inst })
+  test_name =  "Mexico City to ${each.value.agent_name}"
+  target_agent_id = each.value.agent_id
+  interval  = var.test_A2A_interval
+  direction = var.test_A2A_direction
+  protocol = var.test_A2A_protocol
+  bgp_measurements = var.bgp
+  alerts_enabled = var.alerts
+  dynamic "agents" {
+    for_each = local.agentMexicoCity_id
+    content  {
+    agent_id   = agents.value
+    }
+  }
+}
