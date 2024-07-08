4d7eeffb67e54284ef2fa22cdce7b611421d8da5
Edwin Salazar <edwsalaz@cisco.com>
2024-07-08 14:44:32 -0500

    adding IPv6 tests

--- latam/tests.tf
+++ latam/tests.tf
@@ -49,6 +49,23 @@ resource "thousandeyes_agent_to_agent" "Mexico_City" {
   }
 }
 
+resource "thousandeyes_agent_to_agent" "Mexico_City_IPv6" {
+  for_each = tomap({ for inst in local.agentsMexicoCityIPv6From : inst.agent_name => inst })
+  test_name =  "Mexico City IPv6 to ${each.value.agent_name}"
+  target_agent_id = each.value.agent_id
+  interval  = var.test_A2A_interval
+  direction = var.test_A2A_direction
+  protocol = var.test_A2A_protocol
+  bgp_measurements = var.bgp
+  alerts_enabled = var.alerts
+  dynamic "agents" {
+    for_each = local.agentMexicoCityIPv6To_id
+    content  {
+    agent_id   = agents.value
+    }
+  }
+}
+
 resource "thousandeyes_agent_to_agent" "Santiago" {
   for_each = tomap({ for inst in local.agentsSantiagoFrom : inst.agent_name => inst })
   test_name =  "Santiago to ${each.value.agent_name}"
@@ -60,6 +77,23 @@ resource "thousandeyes_agent_to_agent" "Santiago" {
   alerts_enabled = var.alerts
   dynamic "agents" {
     for_each = local.agentSantiagoTo_id
+    content  {
+    agent_id   = agents.value
+    }
+  }
+}
+
+resource "thousandeyes_agent_to_agent" "Santiago_IPv6" {
+  for_each = tomap({ for inst in local.agentsSantiagoIPv6From : inst.agent_name => inst })
+  test_name =  "Santiago IPv6 to ${each.value.agent_name}"
+  target_agent_id = each.value.agent_id
+  interval  = var.test_A2A_interval
+  direction = var.test_A2A_direction
+  protocol = var.test_A2A_protocol
+  bgp_measurements = var.bgp
+  alerts_enabled = var.alerts
+  dynamic "agents" {
+    for_each = local.agentSantiagoIPv6To_id
     content  {
     agent_id   = agents.value
     }
