5e9b059ddca42ca99269790a451a1250170c5fff
Edwin Salazar <edwsalaz@cisco.com>
2024-07-08 14:02:20 -0500

    lima

--- latam/agentsLima.tf
+++ latam/agentsLima.tf
@@ -4,3 +4,14 @@ locals {
   agentLimaFrom_id = keys(local.agentsLimaFrom_id_name)
   agentLimaFrom_name = values(local.agentsLimaFrom_id_name) 
   }
+
+locals {
+  agentsLimaTo = csvdecode(file("${path.module}/agentsLimaTo.csv"))
+  agentsLimaTo_id_name = {for ag_name in local.agentsLimaTo : ag_name.agent_id => ag_name.agent_name}
+  agentLimaTo_id = keys(local.agentsLimaTo_id_name)
+  agentLimaTo_name = values(local.agentsLimaTo_id_name) 
+  }
+
+output "agents_Lima" {
+  value = local.agentsLimaTo_id_name
+}
