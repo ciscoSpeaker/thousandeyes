04a81d574a6787d1a168dee926c3641365bae217
Edwin Salazar <edwsalaz@cisco.com>
2024-07-08 14:05:30 -0500

    lima-ipv6

--- latam/agentsLima.tf
+++ latam/agentsLima.tf
@@ -6,12 +6,30 @@ locals {
   }
 
 locals {
+  agentsLimaIPv6From = csvdecode(file("${path.module}/agentsLimaIPv6From.csv"))
+  agentsLimaIPv6From_id_name = {for ag_name in local.agentsLimaIPv6From : ag_name.agent_id => ag_name.agent_name}
+  agentLimaIPv6From_id = keys(local.agentsLimaIPv6From_id_name)
+  agentLimaIPv6From_name = values(local.agentsLimaIPv6From_id_name) 
+  }
+
+locals {
   agentsLimaTo = csvdecode(file("${path.module}/agentsLimaTo.csv"))
   agentsLimaTo_id_name = {for ag_name in local.agentsLimaTo : ag_name.agent_id => ag_name.agent_name}
   agentLimaTo_id = keys(local.agentsLimaTo_id_name)
   agentLimaTo_name = values(local.agentsLimaTo_id_name) 
   }
 
+locals {
+  agentsLimaIPv6To = csvdecode(file("${path.module}/agentsLimaIPv6To.csv"))
+  agentsLimaIPv6To_id_name = {for ag_name in local.agentsLimaIPv6To : ag_name.agent_id => ag_name.agent_name}
+  agentLimaIPv6To_id = keys(local.agentsLimaIPv6To_id_name)
+  agentLimaIPv6To_name = values(local.agentsLimaIPv6To_id_name) 
+  }
+
 output "agents_Lima" {
   value = local.agentsLimaTo_id_name
+}
+
+output "agents_Lima_IPv6" {
+  value = local.agentsLimaIPv6To_id_name
 }
