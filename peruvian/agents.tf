de8f3c9ff387b8f1b08faea22b0357b33b76b9b3
Edwin Salazar <edwsalaz@cisco.com>
2024-07-08 09:21:41 -0500

    Update agents.tf

--- peruvian/agents.tf
+++ peruvian/agents.tf
@@ -8,3 +8,14 @@ locals {
 output "agents_Lima" {
   value = local.agentsLima_id_name
 }
+
+locals {
+  agentsRPi = jsondecode(file("${path.module}/agentsRPi.json")) 
+  agentsRPi_name_id = {for ag_name in local.agentsRPi.agents : ag_name.agentName => ag_name.agentId}
+  agentsRPiName = keys(local.agentsRPi_name_id)
+  agentsRPiId = values(local.agentsRPi_name_id) 
+  }
+
+output "agents_RPi" {
+  value = local.agentsRPi_name_id
+}
