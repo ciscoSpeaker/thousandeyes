6cf9a417bc21dc4e856cc9e5b49e28e78dd8b23e
Edwin Salazar <edwsalaz@cisco.com>
2024-07-08 09:37:41 -0500

    Update agents.tf

--- peruvian/agents.tf
+++ peruvian/agents.tf
@@ -1,12 +1,12 @@
 locals {
   agentsLima = csvdecode(file("${path.module}/agentsLima.csv"))
-  agentsLima_id_name = {for ag_name in local.agentsLima : ag_name.agent_name => ag_name.agent_id}
-  agentLima_name = keys(local.agentsLima_id_name)
-  agentLima_id = values(local.agentsLima_id_name) 
+  agentsLima_name_id = {for ag_name in local.agentsLima : ag_name.agent_name => ag_name.agent_id}
+  agentLima_name = keys(local.agentsLima_name_id)
+  agentLima_id = values(local.agentsLima_name_id) 
   }
 
 output "agents_Lima" {
-  value = local.agentsLima_id_name
+  value = local.agentsLima_name_id
 }
 
 locals {
@@ -21,7 +21,7 @@ output "agents_RPi" {
 }
 
 locals {
-  agents = merge (local.agentsLima_id_name, local.agentsRPi_name_id)
+  agents = merge (local.agentsLima_name_id, local.agentsRPi_name_id)
 }
 
 output "agents_Lima_RPi" {
