9a07f49d991e85353c46ac2984e78ca6004db9ed
Edwin Salazar <edwin@cisco.com>
2024-10-27 19:04:51 -0400

    transaction tests

--- peruvian/tests.tf
+++ peruvian/tests.tf
@@ -18,24 +18,24 @@ resource "thousandeyes_http_server" "http" {
   }
 }
 
-resource "thousandeyes_dns_server" "dns" {
-  for_each = tomap({ for inst in local.tests : inst.test_resource => inst })
-  test_name      = each.value.test_name
-  interval       = var.test_dns_interval
-  alerts_enabled = var.alerts
-  domain = each.value.test_domain
-  bgp_measurements = var.bgp
-  use_public_bgp = var.bgp
-  dynamic "dns_servers" {
-    for_each = local.server_dns_name_ip
-    content {
-      server_name = dns_servers.value
-    }
-  }
-  dynamic "agents" {
-    for_each = local.agentPeru_id
-    content  {
-    agent_id   = agents.value
-    }
-  }
-}
+#resource "thousandeyes_dns_server" "dns" {
+#  for_each = tomap({ for inst in local.tests : inst.test_resource => inst })
+#  test_name      = each.value.test_name
+#  interval       = var.test_dns_interval
+#  alerts_enabled = var.alerts
+#  domain = each.value.test_domain
+#  bgp_measurements = var.bgp
+#  use_public_bgp = var.bgp
+#  dynamic "dns_servers" {
+#    for_each = local.server_dns_name_ip
+#    content {
+#      server_name = dns_servers.value
+#    }
+#  }
+#  dynamic "agents" {
+#    for_each = local.agentPeru_id
+#    content  {
+#    agent_id   = agents.value
+#    }
+#  }
+#}
