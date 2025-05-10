0e398cff560c356878402f9b96cd32cc41a88e6f
Edwin Salazar <edwin@cisco.com>
2025-05-10 17:41:30 -0400

    Create tests.tf

---
+++ hybridwork/tests.tf
@@ -0,0 +1,50 @@
+resource "thousandeyes_agent_to_agent" "example_agent_to_agent_test" {
+  test_name      = "Example Agent to Agent test set from Terraform provider"
+  interval       = 120
+  alerts_enabled = false
+
+  direction = "TO_TARGET"
+  protocol = "TCP"
+  target_agent_id = "5"
+
+  agents {
+    agent_id = 3 # Singapore
+  }
+}
+
+resource "thousandeyes_agent_to_server" "example_agent_to_server_test" {
+  test_name      = "Example Agent to Server test set from Terraform provider"
+  interval       = 120
+  alerts_enabled = false
+
+  server = "www.thousandeyes.com"
+  port   = 443
+
+  agents {
+    agent_id = 3 # Singapore
+  }
+}
+
+resource "thousandeyes_http_server" "example_http_server_test" {
+  test_name      = "Example HTTP test set from Terraform provider"
+  interval       = 120
+  alerts_enabled = false
+
+  url = "https://www.thousandeyes.com"
+
+  agents {
+    agent_id = 3 # Singapore
+  }
+}
+
+resource "thousandeyes_voice" "Dallas.RTP.webex" {
+  test_name        = "Dallas.RTP.webex"
+  interval         = 120
+  alerts_enabled   = false
+  bgp_measurements = true
+  use_public_bgp   = true
+  target_agent_id = 4 # Tokyo
+  agents {
+    agent_id = 1423278 # Singapore
+  }
+}
