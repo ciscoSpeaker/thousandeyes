cfb991fff6dd95c8490a44d5fb6aa7c85dd7a62f
Edwin Salazar <edwin@cisco.com>
2025-05-12 21:40:00 -0400

    Update testsA2S.tf

--- hybridwork/testsA2S.tf
+++ hybridwork/testsA2S.tf
@@ -1,9 +1,11 @@
 resource "thousandeyes_agent_to_server" "RTP_cisco" {
   test_name      = "RTP.cisco"
-  interval       = 120
-  alerts_enabled = false
+  interval       = 3600
+  alerts_enabled = true
   server = "rtp-vpn-cluster.cisco.com"
   port   = 443
+  bgp_measurements = true
+  use_public_bgp = true 
   agents = [
     1423278,
     69
@@ -12,10 +14,13 @@ resource "thousandeyes_agent_to_server" "RTP_cisco" {
 
 resource "thousandeyes_agent_to_server" "Richardson_cisco" {
   test_name      = "Richardson.cisco"
-  interval       = 120
-  alerts_enabled = false
+  interval       = 3500
+  alerts_enabled = true
   server = "rcdn-vpn-cluster.cisco.com"
+  bgp_measurements = true
   port   = 443
+  bgp_measurements = true
+  use_public_bgp = true 
   agents = [
     1423278,
     69
@@ -24,10 +29,12 @@ resource "thousandeyes_agent_to_server" "Richardson_cisco" {
 
 resource "thousandeyes_agent_to_server" "SanJose_cisco" {
   test_name      = "SanJose.cisco"
-  interval       = 120
-  alerts_enabled = false
+  interval       = 3600
+  alerts_enabled = true
   server = "sjc-vpn-cluster.cisco.com"
   port   = 443
+  bgp_measurements = true
+  use_public_bgp = true 
   agents = [
     1423278,
     69
@@ -36,10 +43,12 @@ resource "thousandeyes_agent_to_server" "SanJose_cisco" {
 
 resource "thousandeyes_agent_to_server" "toparpa_descubre" {
   test_name      = "toparpa.descubre"
-  interval       = 120
-  alerts_enabled = false
+  interval       = 3600
+  alerts_enabled = true
   server = "toparpa.descubre.app"
   port   = 1194
+  bgp_measurements = true
+  use_public_bgp = true 
   agents = [
     1423278,
     69
@@ -48,10 +57,12 @@ resource "thousandeyes_agent_to_server" "toparpa_descubre" {
 
 resource "thousandeyes_agent_to_server" "cary_descubre" {
   test_name      = "cary.descubre"
-  interval       = 120
-  alerts_enabled = false
+  interval       = 3600
+  alerts_enabled = true
   server = "cary.descubre.app"
   port   = 1194
+  bgp_measurements = true
+  use_public_bgp = true 
   agents = [
     1423278,
     69
