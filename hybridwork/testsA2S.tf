be00c4ecaa8c92069a78e769ea3f3ec81ec0c9c3
Edwin Salazar <edwin@cisco.com>
2025-05-12 21:14:14 -0400

    Update testsA2S.tf

--- hybridwork/testsA2S.tf
+++ hybridwork/testsA2S.tf
@@ -1,13 +1,11 @@
-
-#resource "thousandeyes_agent_to_server" "example_agent_to_server_test" {
-#  test_name      = "Example Agent to Server test set from Terraform provider"
-#  interval       = 120
-#  alerts_enabled = false
-#
-#  server = "www.thousandeyes.com"
-#  port   = 443
-#
-#  agents {
-#    agent_id = 3 # Singapore
-#  }
-#}
+resource "thousandeyes_agent_to_server" "RTP_cisco" {
+  test_name      = "RTP.cisco"
+  interval       = 120
+  alerts_enabled = false
+  server = "www.thousandeyes.com"
+  port   = 443
+  agents = [
+    1423278,
+    69
+  ]
+}
