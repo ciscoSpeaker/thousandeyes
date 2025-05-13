0219f6ba6a009044f1f0db00d1cb8af90af6b9f0
Edwin Salazar <edwin@cisco.com>
2025-05-12 21:06:36 -0400

    update

--- hybridwork/testsA2A.tf
+++ hybridwork/testsA2A.tf
@@ -11,16 +11,3 @@
 #    agent_id = 3 # Singapore
 #  }
 #}
-
-#resource "thousandeyes_agent_to_server" "example_agent_to_server_test" {
-#  test_name      = "Example Agent to Server test set from Terraform provider"
-#  interval       = 120
-#  alerts_enabled = false
-#
-#  server = "www.thousandeyes.com"
-#  port   = 443
-#
-#  agents {
-#    agent_id = 3 # Singapore
-#  }
-#}
---
+++ hybridwork/testsA2S.tf
@@ -0,0 +1,13 @@
+
+#resource "thousandeyes_agent_to_server" "example_agent_to_server_test" {
+#  test_name      = "Example Agent to Server test set from Terraform provider"
+#  interval       = 120
+#  alerts_enabled = false
+#
+#  server = "www.thousandeyes.com"
+#  port   = 443
+#
+#  agents {
+#    agent_id = 3 # Singapore
+#  }
+#}
