a4d1ab62158eb3c4857edfa493c4c9878b8e3ddc
Edwin Salazar <edwin@cisco.com>
2025-05-12 22:15:10 -0400

    Update testsA2A.tf

--- hybridwork/testsA2A.tf
+++ hybridwork/testsA2A.tf
@@ -1,10 +1,14 @@
 resource "thousandeyes_agent_to_agent" "Dallas_video_webex" {
   test_name      = "Dallas.video.webex"
-  alerts_enabled = false
+  alerts_enabled = true
   interval = 3600
   direction = "bidirectional"
   protocol = "tcp"
-  target_agent_id = "5"
+  port = 5004
+  target_agent_id = "775091"
+  dscp_id = 34
+  bgp_measurements = true
+  use_public_bgp = true
   agents = [
     1423278,
     69
@@ -13,11 +17,15 @@ resource "thousandeyes_agent_to_agent" "Dallas_video_webex" {
 
 resource "thousandeyes_agent_to_agent" "Chicago_video_webex" {
   test_name      = "Chicago.video.webex"
-  alerts_enabled = false
+  alerts_enabled = true
   interval = 3600
   direction = "bidirectional"
   protocol = "tcp"
-  target_agent_id = "5"
+  port = 5004
+  target_agent_id = "775081"
+  dscp_id = 34
+  bgp_measurements = true
+  use_public_bgp = true
   agents = [
     1423278,
     69
@@ -26,11 +34,15 @@ resource "thousandeyes_agent_to_agent" "Chicago_video_webex" {
 
 resource "thousandeyes_agent_to_agent" "Chicago_SIP_webex" {
   test_name      = "Chicago.SIP.webex"
-  alerts_enabled = false
+  alerts_enabled = true
   interval = 3600
   direction = "bidirectional"
   protocol = "tcp"
-  target_agent_id = "5"
+  port = 5062
+  target_agent_id = "775081"
+  dscp_id = 34
+  bgp_measurements = true
+  use_public_bgp = true
   agents = [
     1423278,
     69
@@ -39,11 +51,15 @@ resource "thousandeyes_agent_to_agent" "Chicago_SIP_webex" {
 
 resource "thousandeyes_agent_to_agent" "Dallas_SIP_webex" {
   test_name      = "Dallas_SIP_webex"
-  alerts_enabled = false
+  alerts_enabled = true
   interval = 3600
   direction = "bidirectional"
   protocol = "tcp"
-  target_agent_id = "5"
+  port = 5062
+  target_agent_id = "775091"
+  dscp_id = 34
+  bgp_measurements = true
+  use_public_bgp = true
   agents = [
     1423278,
     69
