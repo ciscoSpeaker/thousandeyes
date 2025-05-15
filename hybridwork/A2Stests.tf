0b61ea938540a6da7d9f39a1e087ffbcc0777a15
Edwin Salazar <edwin@cisco.com>
2025-05-15 18:38:27 -0400

    a2s tests update

--- hybridwork/A2Stests.tf
+++ hybridwork/A2Stests.tf
@@ -7,67 +7,20 @@ locals {
 #  value = local.A2Salerts_id
 #}
 
-resource "thousandeyes_agent_to_server" "RTP_cisco" {
-  test_name      = "RTP.cisco"
-  server = "rtp-vpn-cluster.cisco.com"
-  port   = 443
-  interval       = var.a2s_test_interval       
-  bgp_measurements = var.a2s_test_bgp 
-  use_public_bgp = var.a2s_test_use_public_bgp 
-  enabled =  var.a2s_test_enabled       
-  alerts_enabled = var.a2s_test_alerts_enabled 
-  alert_rules = local.A2Salerts_id
-  agents = local.agents_id
+locals {
+  A2Stests = csvdecode(file("${path.module}/A2Stests.csv"))
 }
 
-resource "thousandeyes_agent_to_server" "Richardson_cisco" {
-  test_name      = "Richardson.cisco"
-  server = "rcdn-vpn-cluster.cisco.com"
-  port   = 443
-  interval       = var.a2s_test_interval       
-  bgp_measurements = var.a2s_test_bgp 
-  use_public_bgp = var.a2s_test_use_public_bgp 
-  enabled =  var.a2s_test_enabled       
-  alerts_enabled = var.a2s_test_alerts_enabled 
-  alert_rules = local.A2Salerts_id
-  agents = local.agents_id
-}
-
-resource "thousandeyes_agent_to_server" "SanJose_cisco" {
-  test_name      = "SanJose.cisco"
-  server = "sjc-vpn-cluster.cisco.com"
-  port   = 443
-  interval       = var.a2s_test_interval       
-  bgp_measurements = var.a2s_test_bgp 
-  use_public_bgp = var.a2s_test_use_public_bgp 
-  enabled =  var.a2s_test_enabled       
-  alerts_enabled = var.a2s_test_alerts_enabled 
-  alert_rules = local.A2Salerts_id
-  agents = local.agents_id
-}
-
-resource "thousandeyes_agent_to_server" "toparpa_descubre" {
-  test_name      = "toparpa.descubre"
-  server = "toparpa.descubre.app"
-  port   = 1194
-  interval       = var.a2s_test_interval       
-  bgp_measurements = var.a2s_test_bgp 
-  use_public_bgp = var.a2s_test_use_public_bgp 
-  enabled =  var.a2s_test_enabled       
-  alerts_enabled = var.a2s_test_alerts_enabled 
-  alert_rules = local.A2Salerts_id
-  agents = local.agents_id
-}
-
-resource "thousandeyes_agent_to_server" "cary_descubre" {
-  test_name      = "cary.descubre"
-  server = "cary.descubre.app"
-  port   = 1194
-  interval       = var.a2s_test_interval       
-  bgp_measurements = var.a2s_test_bgp 
-  use_public_bgp = var.a2s_test_use_public_bgp 
-  enabled =  var.a2s_test_enabled       
-  alerts_enabled = var.a2s_test_alerts_enabled 
-  alert_rules = local.A2Salerts_id
-  agents = local.agents_id
+resource "thousandeyes_agent_to_server" "a2s" {
+  for_each          = tomap({ for inst in local.A2Stests : inst.test_resource => inst })
+  test_name         = each.value.test_name  
+  server            = each.value.server
+  port              = tonumber(each.value.port)
+  interval          = var.a2s_test_interval       
+  bgp_measurements  = var.a2s_test_bgp 
+  use_public_bgp    = var.a2s_test_use_public_bgp 
+  enabled           = var.a2s_test_enabled       
+  alerts_enabled    = var.a2s_test_alerts_enabled 
+  alert_rules       = local.A2Salerts_id
+  agents            = local.agents_id
 }
