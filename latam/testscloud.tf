7b11cbe4f59368776e9060b8c7f9a1f726ff0bcf
Edwin Salazar <edwin@cisco.com>
2025-05-15 19:39:50 -0400

    variables

--- latam/testscloud.tf
+++ latam/testscloud.tf
@@ -1,17 +1,17 @@
-resource "thousandeyes_agent_to_agent" "Cloud" {
-  for_each = tomap({ for inst in local.agentsCloudTo : inst.agent_name => inst })
-  test_name =  "Latam to ${each.value.agent_name}"
-  target_agent_id = each.value.agent_id
-  enabled = var.test_enabled
-  interval  = var.test_A2A_interval
-  direction = var.test_A2A_direction
-  protocol = var.test_A2A_protocol
-  bgp_measurements = var.bgp
-  use_public_bgp = var.bgp
-  alerts_enabled = var.alerts
-  dscp_id = var.dscp
-  #alert_rules = local.A2Aalerts_id
-  #agents = local.agentCloudFrom_id
+resource "thousandeyes_agent_to_agent" "cloud" {
+  for_each          = tomap({ for inst in local.agentsCloudTo : inst.agent_name => inst })
+  test_name         =  "latam to ${each.value.agent_name}"
+  target_agent_id   = each.value.agent_id
+  enabled           = var.test_enabled
+  interval          = var.test_A2A_interval
+  direction         = var.test_A2A_direction
+  protocol          = var.test_A2A_protocol
+  bgp_measurements  = var.bgp
+  use_public_bgp    = var.bgp
+  alerts_enabled    = var.alerts
+  dscp_id           = var.dscp
+  #alert_rules      = local.A2Aalerts_id
+  #agents           = local.agentCloudFrom_id
   dynamic "agents" {
     for_each = local.agentCloudFrom_id
     content  {
---
+++ latam/variables.auto.tfvars
@@ -0,0 +1,12 @@
+te_timeout = 1000
+
+test_enabled = true
+alerts       = true
+bgp          = true
+dscp         = 46
+
+test_A2A_direction = BIDIRECTIONAL
+test_A2A_interval = 3600
+test_A2A_protocol = TCP
+
+test_http_interval = 3600
