671501e9060db53bda297a2fda0dd7b0b450c1e5
Edwin Salazar <edwin@cisco.com>
2025-05-15 20:03:40 -0400

    variables

--- peruvian/tests.tf
+++ peruvian/tests.tf
@@ -3,12 +3,12 @@ locals {
   }
 
 resource "thousandeyes_http_server" "http" {
-  for_each = tomap({ for inst in local.tests : inst.test_resource => inst })
-  test_name =  each.value.test_name
-  interval       = var.test_http_interval
-  alerts_enabled = var.alerts
-  url = each.value.test_url
-  enabled = var.test_enabled
+  for_each            = tomap({ for inst in local.tests : inst.test_resource => inst })
+  test_name           =  each.value.test_name
+  interval            = var.test_http_interval
+  alerts_enabled      = var.alerts
+  url                 = each.value.test_url
+  enabled             = var.test_enabled
   # bgp_measurements = var.bgp
   # use_public_bgp = var.bgp
   #agents = local.agentRPi_id
@@ -27,12 +27,12 @@ resource "thousandeyes_http_server" "http" {
 }
 
 resource "thousandeyes_dns_trace" "dns" {
-  for_each       = { for test in local.tests : test.test_resource => test }
-  test_name      = each.value.test_name
-  interval       = var.test_dns_interval
-  alerts_enabled = var.alerts
-  domain         = each.value.test_domain
-  enabled        = var.test_enabled
+  for_each            = { for test in local.tests : test.test_resource => test }
+  test_name           = each.value.test_name
+  interval            = var.test_dns_interval
+  alerts_enabled      = var.alerts
+  domain              = each.value.test_domain
+  enabled             = var.test_enabled
   dynamic "agents" {
     for_each = local.agentRPi_id
     content  {
---
+++ peruvian/variables.auto.tfvars
@@ -0,0 +1,12 @@
+te_timeout = 1000
+
+alerts  = true
+bgp = true
+test_dns_interval = 1800
+test_enabled = true
+test_http_interval = true
+test_transaction_interval = 3600
+test_vpn_interval = 3600
+target_time = 60
+time_limit = 120
+user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.5993.70 Safari/537.36"
