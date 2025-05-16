6e9d34dee3541a58d6f2c3a0f51e63675da2d6d8
Edwin Salazar <edwin@cisco.com>
2025-05-15 20:33:15 -0400

    bgp

---
+++ peruvian/bgpmonitor.tf
@@ -0,0 +1,14 @@
+locals {
+  bgpmonitor = csvdecode(file("${path.module}/bgpmonitor.csv"))
+  bgpmonitor_id_name = {for bgp in local.bgpmonitor : bgp.monitorId => bgp.monitorName}
+  bgpmonitor_id = keys(local.bgpmonitor_id_name)
+  bgpmonitor_name = values(local.bgpmonitor_id_name)
+}
+
+output "bgpmonitor_id" {
+  value = local.bgpmonitor_id
+}
+
+output "bgpmonitor_name" {
+  value = local.bgpmonitor_name
+}
