51def043c438b52ed38606a56c3a32a99948ee4c
Edwin Salazar <edwin@cisco.com>
2025-05-16 15:24:31 -0400

    tests http code

--- peruvian/tests.tf
+++ peruvian/tests.tf
@@ -1,12 +1,14 @@
 
 resource "thousandeyes_http_server" "http" {
-  for_each            = tomap({ for inst in local.tests : inst.test_resource => inst })
-  test_name           = each.value.test_name
-  interval            = var.test_http_interval
-  alerts_enabled      = var.alerts
-  url                 = each.value.test_url
-  enabled             = var.test_enabled
-  bgp_measurements    = var.bgp
+  for_each             = tomap({ for inst in local.tests : inst.test_resource => inst })
+  test_name            = each.value.test_name
+  interval             = var.test_http_interval
+  alerts_enabled       = var.alerts
+  url                  = each.value.test_url
+  content_regex        = each.value.content_regex
+  desired_status_code  = tonumber(each.value.desired_status_code)
+  enabled              = var.test_enabled
+  bgp_measurements     = var.bgp
   #agents = local.agentRPi_id
   dynamic "agents" {
     for_each = local.agentPeru_id
