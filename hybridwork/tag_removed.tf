0b4ebc7ff197b6c0e61866a440a43cb13abf9c96
Edwin Salazar <edwin@iartificial.io>
2026-03-18 20:01:23 -0500

    tag fixing

---
+++ hybridwork/tag_removed.tf
@@ -0,0 +1,31 @@
+removed {
+  from = thousandeyes_tag.csv_tags["thousandeyes_service:ciscotools:test"]
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = thousandeyes_tag.csv_tags["thousandeyes_service:collaboration:endpoint-test"]
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = thousandeyes_tag.csv_tags["thousandeyes_service:collaboration:test"]
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = thousandeyes_tag.csv_tags["thousandeyes_service:vpn:test"]
+
+  lifecycle {
+    destroy = false
+  }
+}
