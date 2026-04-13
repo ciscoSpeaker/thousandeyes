66926ffa728d34fcd607ff651f7aacb8f418ad0c
edwin <edwin@cisco.com>
2026-04-13 01:37:11 -0500

    time execution update

--- peruvian/executiontime.tf
+++ peruvian/executiontime.tf
@@ -1,21 +1,3 @@
-#locals {
-#  timenow = timestamp()
-#  now = formatdate("hh:mm:ss ZZZ - MMM/DD/YYYY", local.timenow)
-#}
-
-#output "execution_time" {
-#  value = local.timenow
-#}
-
-terraform {
-  required_providers {
-    time = {
-      source  = "hashicorp/time"
-      version = "~> 0.12"
-    }
-  }
-}
-
 resource "time_static" "execution_time" {}
 
 output "execution_time" {
--- peruvian/terraform.tf
+++ peruvian/terraform.tf
@@ -1,5 +1,4 @@
 terraform {
-
   cloud {
     organization = "thousandeyes"
 
@@ -10,12 +9,15 @@ terraform {
 
   required_providers {
     thousandeyes = {
-      source = "thousandeyes/thousandeyes"
+      source  = "thousandeyes/thousandeyes"
       version = "= 2.0.10"
-      #version = ">= 3.0.0"
+    }
+
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.12"
     }
   }
 
   required_version = ">= 0.14.0"
-  #required_version = ">= 1.3.0"
 }
